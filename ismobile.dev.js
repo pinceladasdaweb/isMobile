2d8b35c6f093260bc3e00ef67062ecb5e5f3354f
Gustavo Franco <gustavocfranco@gmail.com>
2013-08-18 23:33:45 -0300

    Code optimized

--- ismobile.dev.js
+++ ismobile.dev.js
@@ -9,14 +9,27 @@ isMobile
 + Licensed under the MIT license
 */
 
-var isMobile = (function () {
-    var ua = navigator.userAgent.toLowerCase(),
-        agents = ['android', 'webos', 'iphone', 'ipad', 'ipod', 'opera mini', 'opera mobi', 'opera tablet', 'iemobile', 'windows phone', 'blackberry', 'bb10; touch'],
-        device;
+;var isMobile = (function () {
+  'use strict';
 
-    for (device in agents) {
-        if (ua.indexOf(agents[device]) > -1) {
-            return true;
-        }
+  var userAgent = navigator.userAgent.toLowerCase(),
+      mobileUserAgents = ['android',
+                          'webos',
+                          'iphone',
+                          'ipad',
+                          'ipod',
+                          'opera mini',
+                          'opera mobi',
+                          'opera tablet',
+                          'iemobile',
+                          'windows phone',
+                          'blackberry',
+                          'bb10; touch'];
+  for (var index = 0, length = mobileUserAgents.length; index < length; index++) {
+    if (userAgent.indexOf(mobileUserAgents[index]) !== -1) {
+      return true;
     }
+  }
+
+  return false;
 }());
