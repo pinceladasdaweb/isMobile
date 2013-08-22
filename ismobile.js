ecf4a73748f0869f4b6a3b7b31f1c2e67c4211d4
Pedro Rogério <pinceladasdaweb@hotmail.com>
2013-08-21 22:12:24 -0300

    Update userAgent match

--- ismobile.js
+++ ismobile.js
@@ -4,7 +4,7 @@ isMobile
 --------------------------------
 + https://github.com/pinceladasdaweb/isMobile
 + Detect Mobile Browsers with pure JavaScript
-+ version 1.0
++ version 2.0
 + Copyright 2013 Pedro Rogerio
 + Licensed under the MIT license
 */
@@ -12,5 +12,5 @@ isMobile
 ;var isMobile = (function (userAgent) {
   'use strict';
 
-  return !!userAgent.match(/android|webos|ip(hone|ad|od)|opera (mini|mobi|tablet)|iemobile|windows phone|mobile|fennec|kindle|maemo|blackberry|playbook|bb10\; (touch|kbd)/i);
+  return !!userAgent.match(/android|webos|ip(hone|ad|od)|opera (mini|mobi|tablet)|iemobile|windows.+(phone|touch)|mobile|fennec|kindle (Fire)|Silk|maemo|blackberry|playbook|bb10\; (touch|kbd)/i);
 }(navigator.userAgent));
